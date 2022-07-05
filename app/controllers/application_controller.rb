72f044704fa4822502e8e9a2a285e7467bfdb951
Aaron Lally <aaronblally@gmail.com>
2022-07-05 16:08:28 -0400

    commit tree 7d01837bc2f09c28de29197d55c855dad6faecbd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
+def render_not_found(e)
+  render json: { error: "#{e.model} not found" }, status: :not_found
+end
+
+def render_unprocessable_entity(e)
+  render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+end
 end
